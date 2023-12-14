378ff6815f7ff21734df5bd1ee28ca3e325d34cb
MrShadowDani <dgonzsan@myuax.com>
2023-12-14 12:06:00 +0100

    guardar

--- ejercicio/main.cpp
+++ ejercicio/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Estudiante.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Estudiante estudiante;
+    estudiante.nombre = "Juan";
+    estudiante.edad = 20;
+    estudiante.promedio = 8.5;
+    estudiante.materias = {"Matematicas", "Historia", "Fisica"};
+
+    mostrarEstudiante(estudiante);
+
     return 0;
 }
