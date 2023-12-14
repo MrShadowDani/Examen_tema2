ad9f499124dfff754d83327073b98b5c5a69e6fd
MrShadowDani <dgonzsan@myuax.com>
2023-12-14 12:41:22 +0100

    guardar

--- ejercicio/main.cpp
+++ ejercicio/main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
 #include "Estudiante.h"
+
+void Estudiante::agregarMateria(const std::string& materia) {
+    materias.push_back(materia);
+}
+
+void Estudiante::eliminarMateria(const std::string& materia) {
+    // Eliminar la materia si existe
+    auto it = std::find(materias.begin(), materias.end(), materia);
+    if (it != materias.end()) {
+        materias.erase(it);
+    }
+}
+
+void Estudiante::mostrarMaterias() {
+    std::cout << "Materias: ";
+    for (const auto& materia : materias) {
+        std::cout << materia << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+    estudiante.mostrarMaterias();
+}
 
 int main() {
     Estudiante estudiante;
@@ -7,6 +37,16 @@ int main() {
     estudiante.promedio = 8.5;
     estudiante.materias = {"Matematicas", "Historia", "Fisica"};
 
+    mostrarEstudiante(estudiante);
+
+    // Agregar una materia
+    estudiante.agregarMateria("Quimica");
+    std::cout << "Después de agregar Quimica:" << std::endl;
+    mostrarEstudiante(estudiante);
+
+    // Eliminar una materia
+    estudiante.eliminarMateria("Historia");
+    std::cout << "Después de eliminar Historia:" << std::endl;
     mostrarEstudiante(estudiante);
 
     return 0;
