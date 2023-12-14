941dfdde544a903bec2448268c6c5a3344811710
MrShadowDani <dgonzsan@myuax.com>
2023-12-14 13:18:33 +0100

    guardar

--- ejercicio en c/main.c
+++ ejercicio en c/main.c
@@ -1,8 +1,10 @@
-#include "Estudiante_C.h" // Incluye el archivo de encabezado que contiene las definiciones de las funciones y la estructura Estudiante
+#include "Estudiante_C.h"
+#include "Asistencia_C.h"
+#include <stdio.h>
 
 int main() {
     // Creación de un estudiante y asignación de valores iniciales
-    struct Estudiante estudiante1 = {"Juan", 20, 8.5f, (char*[]){"Matematicas", "Historia"}, 2};
+    struct Estudiante estudiante1 = {"Juan", 20, 8.5f, (char*[]){"Matemáticas", "Historia"}, 2};
 
     // Mostrar detalles del estudiante utilizando la función mostrarEstudiante
     mostrarEstudiante(estudiante1);
@@ -19,6 +21,18 @@ int main() {
     // Mostrar las materias actualizadas después de eliminar Historia
     mostrarMaterias(estudiante1);
 
+    // Crear un registro de asistencias
+    struct RegistroAsistencia registro;
+    registro.asistencias = NULL; // Inicializar el registro sin asistencias
+    registro.num_asistencias = 0;
+
+    // Registrar una asistencia
+    registrarAsistencia(&registro, "2023-12-15", "Matemáticas", "Presente");
+
+    // Mostrar las asistencias registradas
+    mostrarAsistencias(registro);
+
     return 0;
 }
+
 
