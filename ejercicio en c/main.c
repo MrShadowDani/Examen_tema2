e165714b899c32cfb3a5dedfae7f4df0f40135ba
MrShadowDani <dgonzsan@myuax.com>
2023-12-14 12:22:07 +0100

    guardar

--- ejercicio en c/main.c
+++ ejercicio en c/main.c
@@ -1,7 +1,7 @@
 #include "Estudiante_C.h"
 
 int main() {
-    struct Estudiante estudiante1 = {"Juan", 20, 8.5, (char*[]){"Matematicas", "Historia"}, 2};
+    struct Estudiante estudiante1 = {"Juan", 20, 8.5f, (char*[]){"Matematicas", "Historia"}, 2};
     mostrarEstudiante(estudiante1);
 
     return 0;
